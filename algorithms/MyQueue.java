605d8c6e2c1d68d6cf0efcea4ef5c07f48740d15
podstanar0 <84190083+podstanar0@users.noreply.github.com>
2021-05-14 16:28:52 +0200

    Add files via upload

---
+++ algorithms/MyQueue.java
@@ -0,0 +1,46 @@
+package algorithms;
+
+ // TASK: implement queue using two stacks
+
+public class MyQueue
+{
+    Stack stackNew, stackOld;
+
+    public MyQueue()
+    {
+        stackNew = new Stack();
+        stackOld = new Stack();
+    }
+
+    public int size()
+    {
+        return stackNew.size() + stackOld.size();
+    }
+
+    public void add(int item)
+    {
+        // push onto stackNew, which always has the newest element on top
+        stackNew.push(item);
+    }
+
+    // move elements from stackNew into stackOld.
+    private void shiftStacks() 
+    {
+        if (stackOld.isEmpty())
+        {
+            while (!stackNew.isEmpty()) stackOld.push(stackNew.pop());
+        }
+    }
+
+    public int peek()
+    {
+        shiftStacks(); // ensures that stackOld has current elements
+        return stackOld.peek(); // retrieve the oldest item
+    }
+
+    public int remove()
+    {
+        shiftStacks(); // ensures that stackOld has current elements
+        return stackOld.pop(); // pop the oldest item
+    }
+}
